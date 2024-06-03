edf7e5a8c232a477110cae27624cd2f3b2db2821
Konrad <K.Gniaz@student.tudelft.nl>
2024-06-03 11:45:14 +0200

    add documentation

--- PluginSubmissionTests.py
+++ PluginSubmissionTests.py
@@ -1,18 +1,27 @@
 import yaml
 
+# tests whether the submission file contains the repository link
+# input - data extracted from the submission.yml file
 def includesRepositoryLinkTest(data):
       try:
             if "/" not in data.get("repository"):
                   raise Exception("No repository name was provided")
+                  return -1
       except:
             raise Exception("No repository name was provided")
+            return -1
 
+# tests whether the submission file contains the variable list
+# input - data extracted from the submission.yml file
 def containsVariablesTest(data):
       try:
             data.get("variables")
       except:
             raise Exception("No variable names were provided")
+            return -1
 
+# tests whether the submission file defines the vital discharge variables within its variable list
+# input - data extracted from the submission.yml file
 def definesDischargeTest(data):
             temp = data.get("variables")
             condition = True
@@ -21,9 +30,13 @@ def definesDischargeTest(data):
                       temp2.get('discharge')
                 except:
                       raise Exception("The discharge variable was not provided")
+                      return -1
 
-data = yaml.safe_load(open('submission.yml'))
+# extract data
+data = yaml.safe_load(open('workflows/submission.yml'))
+# test data
 includesRepositoryLinkTest(data)
 containsVariablesTest(data)
 definesDischargeTest(data)
+# print repository link so that workflow can clone it
 print(data.get("repository"))
