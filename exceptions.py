15246c6e67baa7c55e425fd1de29465c93d46eb8
maselko13 <74785596+maselko13@users.noreply.github.com>
2024-06-21 20:04:00 +0200

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,9 @@
+class VarDoesntExistException(Exception):
+    def __init__(self, message):
+            super().__init__(message)
+class NotFoundException(Exception):
+        def __init__(self, message):
+            super().__init__(message)
+class WrongFormatException(Exception):
+        def __init__(self, message):
+            super().__init__(message)
